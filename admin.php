daa272b531042ef5fbac0ce0b08a882905145d19
IDVSE <idvse@idvse.com>
2013-10-11 15:15:29 +0700

    V1.2.1

--- admin.php
+++ admin.php
@@ -10,8 +10,14 @@ require_once 'db.php';
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-	<title>URL Shortener Script</title>
-	<link rel="stylesheet" type="text/css" href="styles.css"/>
+	<title>Admin | iShortener</title>
+	<meta name="description" content="Your_Description" />
+    <meta name="keywords" content="Your_Keywords" />
+	<meta name="author" content="IDVSE Developer, iDeveloper" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css" />
+    <link rel="stylesheet" type="text/css" href="./css/aquaadmin.css" />
 </head>
 <body>
 
@@ -40,14 +46,32 @@ if (!isset($_SESSION['admin']) OR $_SESSION['admin'] != 'xyz0') {
         }
     }
 ?>
-	<form method="post">
-		Username : <br/>
-		<input type="text" name="u"/><br/>
-		Password : <br />
-		<input type="password" name="p"/><br/>
-		<input type="submit" name="sb" value="Login" />
-	</form>
-	<?php
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12 content">
+                    <div id="content">
+                        <div class="panel login">
+                            <div class="panel-header">
+                                <i class="icon-user icon-blue"></i>
+                                <h2>Login form</h2>
+                            </div>
+                            <div class="panel-content">
+                                <form method="post" />
+                                    <fieldset>
+	                                	Username : <br/>
+	                                	<input type="text" name="u"/><br/>
+	                                	Password : <br />
+	                                	<input type="password" name="p"/><br/>
+										<hr />
+                                        <a class="btn btn-large" href="./"><i class="icon-arrow-left icon-blue"></i> Back to Home</a>
+                                        <button class="btn btn-primary btn-large" type="submit" name="sb"><i class="icon-ok icon-white"></i> Login</button>
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div><!--/span-->
+<?php
 } else {
     if (isset($_GET['delID'])) {
         $id = intval($_GET['delID']);
@@ -55,13 +79,72 @@ if (!isset($_SESSION['admin']) OR $_SESSION['admin'] != 'xyz0') {
         header("Location: admin.php");
         exit;
     }
-    print '<a href="?out=logout">Log Out!</a><br/><hr/><br/>';
+    print '<a class="btn btn-inverse" href="?out=logout"><i class="icon-off"></i> Log Out!</a><br/><hr/><br/>';
     $rs = mysql_query("SELECT * FROM links ORDER BY linkID DESC");
     if (mysql_num_rows($rs)) {
 ?>
-			<h3>A total of <em><?php
+			<script type="text/javascript" src="js/jquery.min.js"></script>
+			<div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="#">Admin Panel</a>
+                    <div class="nav-collapse">
+                        <ul class="nav">
+                            <li class="active"><a href="./"><i class="icon-home icon-aqua"></i> Home</a></li>
+                            <li><a href="#contact"><i class="icon-envelope icon-aqua"></i> Contact</a></li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12 top-bar">
+                    <ul class="breadcrumb">
+                        <li><a href="index.html">Home</a></li><li class="breadcrumb-separator"><i class="icon-chevron-right icon-blue"></i></li> <li><span>Table</span></li></ul><!-- breadcrumbs -->
+                    <div class="actions-bar">
+                        <div class="btn-group pull-right">
+                            <a class="btn btn-inverse"><i class="icon-cog icon-white"></i> Settings</a>
+                            <a class="btn btn-inverse"><i class="icon-edit icon-white"></i> Actions</a>
+                            <a class="btn btn-inverse dropdown-toggle" data-toggle="dropdown">
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#"><i class="icon-plus-sign"></i> Do something</a></li>
+                                <li><a href="#"><i class="icon-cog"></i> Something else</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="span2 sp2 all-left">
+                    <div class="sidebar-nav">
+                        <ul class="nav nav-list">
+                            <li><a href="index.html"><i class="icon-home icon-aqua"></i> Home</a></li>
+                            <li><a href="charts.html"><i class="icon-signal icon-aqua"></i> Charts</a></li>
+                            <li><a href="form.html"><i class="icon-tasks icon-aqua"></i> Forms</a></li>
+                            <li class="active"><a href="table.html"><i class="icon-list-alt icon-aqua"></i> Table</a></li>
+                            <li><a href="errorpage.html"><i class="icon-exclamation-sign icon-aqua"></i> Error</a></li>
+                            <li><a href="gallery.html"><i class="icon-picture icon-aqua"></i> Gallery</a></li>
+                            <li class="nav-header">Another menu</li>
+                            <li><a href="loginform.html"><i class="icon-user icon-aqua"></i> Login form</a></li>
+                        </ul>
+                    </div>
+                </div><!--/span-->
+                <div class="span10 content">
+                    <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="/netbeans/bootstrap/js/excanvas.min.js"></script><![endif]-->
+                    <div id="content">
+                        <div class="span11 panel">
+                            <div class="panel-header">
+                                <i class="icon-list-alt icon-blue"></i>
+                                <h2>A total of <b><em><?php
         print mysql_num_rows($rs);
-?></em> links shortened</h3>
+?></em></b> links shortened</h2>
+                            </div>
 			<table width="640">
 				<tr style="font-weight: bold">
 					<td width="250">Destination URL</td>
@@ -86,7 +169,7 @@ if (!isset($_SESSION['admin']) OR $_SESSION['admin'] != 'xyz0') {
 					  <td>http://' . $_SERVER['SERVER_NAME'] . '/' . $row->string . '</td>
 					  <td>' . date('d/m/Y H:iA', $row->added) . '</td>
 					  <td>' . long2ip($row->ip) . '</td>
-					  <td><a href="?delID=' . $row->linkID . '">[x]</a></td>
+					  <td><a href="?delID=' . $row->linkID . '"><a class="btn btn-small btn-danger"><i class="icon-remove"></i></a></a></td>
 					  </tr>';
         }
 ?>
@@ -95,6 +178,12 @@ if (!isset($_SESSION['admin']) OR $_SESSION['admin'] != 'xyz0') {
     }
 }
 ?>
+                            
+                    </div><!-- content -->
+                </div>
+            </div><!--/row-->
+			
+			
 	
 </div>
 	
