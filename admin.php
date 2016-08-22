dd8115ffb62c0e81f231942c73e5622ebe425f34
Doness <idvse@idvse.com>
2016-08-22 23:31:20 +0700

    iVersion 3.0
    New Template
    Add New Ite Upload (Soon)

--- admin.php
+++ admin.php
@@ -15,9 +15,10 @@ require_once 'db.php';
     <meta name="keywords" content="Your_Keywords" />
 	<meta name="author" content="IDVSE Developer, iDeveloper" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css" />
-    <link rel="stylesheet" type="text/css" href="./css/aquaadmin.css" />
+	<link rel="stylesheet" type="text/css" href="./css/materialize.min.css" />
+    <link rel="stylesheet" type="text/css" href="./css/materialize.css" />
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
 
@@ -52,13 +53,13 @@ if (!isset($_SESSION['admin']) OR $_SESSION['admin'] != 'xyz0') {
             <div class="row-fluid">
                 <div class="span12 content">
                     <div id="content">
-                        <div class="panel login">
-                            <div class="panel-header">
-                                <i class="icon-user icon-blue"></i>
-                                <h2>Login form</h2>
-                            </div>
-                            <div class="panel-content">
-                                <form method="post" />
+					<center>
+					<div class="row">
+						<div class="col s12 m6">
+						  <div class="card blue-grey darken-1">
+							<div class="card-content white-text">
+							  <span class="card-title">Login form</span>
+							  <form method="post" />
                                     <fieldset>
 	                                	Username : <br/>
 	                                	<input type="text" name="u"/><br/>
@@ -69,122 +70,76 @@ if (!isset($_SESSION['admin']) OR $_SESSION['admin'] != 'xyz0') {
                                         <button class="btn btn-primary btn-large" type="submit" name="sb"><i class="icon-ok icon-white"></i> Login</button>
                                     </fieldset>
                                 </form>
-                            </div>
-                        </div><!--/span-->
+							</div>
+						  </div>
+						</div>
+					</div>
+					</center>
 <?php
 } else {
-    if (isset($_GET['delID'])) {
-        $id = intval($_GET['delID']);
-        mysql_query("DELETE FROM links WHERE linkID = $id") or die(mysql_error());
-        header("Location: admin.php");
-        exit;
-    }
-    print '<a class="btn btn-inverse" href="?out=logout"><i class="icon-off"></i> Log Out!</a><br/><hr/><br/>';
-    $rs = mysql_query("SELECT * FROM links ORDER BY linkID DESC");
-    if (mysql_num_rows($rs)) {
+	if (isset($_GET['delID'])) {
+		$id = intval($_GET['delID']);
+		mysql_query("DELETE FROM links WHERE linkID = $id") or die(mysql_error());
+		header("Location: admin.php");
+		exit;
+	}
+	print '<div class="fixed-action-btn" style="bottom: 45px; right: 24px;">';
+	print '<a class="btn-floating btn-large waves-effect waves-light red" href="?out=logout"><i class="material-icons">power_settings_new</i></a><br/>';
+	print '</div>';
+	$rs = mysql_query("SELECT * FROM links ORDER BY linkID DESC");
+	if (mysql_num_rows($rs)) {
 ?>
-			<script type="text/javascript" src="js/jquery.min.js"></script>
-			<div class="navbar navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container-fluid">
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
-                    <a class="brand" href="#">Admin Panel</a>
-                    <div class="nav-collapse">
-                        <ul class="nav">
-                            <li class="active"><a href="./"><i class="icon-home icon-aqua"></i> Home</a></li>
-                            <li><a href="about.html"><i class="icon-user icon-aqua"></i> About</a></li>
-                            <li><a href="contact.php"><i class="icon-envelope icon-aqua"></i> Contact</a></li>
-							<li><a href="tos.html"><i class="icon-exclamation-sign icon-aqua"></i> TOS</a></li>
-							<li><a href="privacy.html"><i class="icon-info-sign icon-aqua"></i> Privacy policy</a></li>
-                        </ul>
-                    </div><!--/.nav-collapse -->
-                </div>
-            </div>
+	<div class="row">
+        <div class="col s12">
+			<div class="card blue-grey darken-1">
+				<div class="card-content white-text">
+					<span class="card-title">
+					A total of 
+					<b>
+					<em><?php
+						print mysql_num_rows($rs);
+						?>
+					</em>
+					</b>  links shortened</span>
+					<table width="640">
+						<tr style="font-weight: bold">
+							<td width="250">Destination URL</td>
+							<td width="200">Shortened</td>
+							<td width="100">Added</td>
+							<td width="70">IP</td>
+							<td width="20">Remove</td>
+						</tr>
+					<?php
+							$i = 0;
+							while ($row = mysql_fetch_object($rs)) {
+								$i++;
+								if ($i % 2 != 0) {
+									$rowColor = "#e9e9e9";
+								} else {
+									$rowColor = "#cccccc";
+								}
+								print '<tr bgcolor="' . $rowColor . '">
+										  <td><a href="http://' . $row->destination . '" target="_blank">
+											' . wordwrap($row->destination, 40, '<br/>', true) . '
+											 </a>
+										  <td>http://' . $_SERVER['SERVER_NAME'] . '/' . $row->string . '</td>
+										  <td>' . date('d/m/Y H:iA', $row->added) . '</td>
+										  <td>' . long2ip($row->ip) . '</td>
+										  <td><a href="?delID=' . $row->linkID . '"><a class="btn btn-small btn-danger"><i class="material-icons">delete</i></a></a></td>
+										  </tr>';
+							}
+					?>
+					</table>
+					<?php
+						}
+					}
+					?>
+				</div>
+          </div>
         </div>
-        <div class="container-fluid">
-            <div class="row-fluid">
-                <div class="span12 top-bar">
-                    <ul class="breadcrumb">
-                        <li><a href="index.html">Home</a></li><li class="breadcrumb-separator"><i class="icon-chevron-right icon-blue"></i></li> <li><span>Table</span></li></ul><!-- breadcrumbs -->
-                    <div class="actions-bar">
-                        <div class="btn-group pull-right">
-                            <a class="btn btn-inverse"><i class="icon-cog icon-white"></i> Settings</a>
-                            <a class="btn btn-inverse"><i class="icon-edit icon-white"></i> Actions</a>
-                            <a class="btn btn-inverse dropdown-toggle" data-toggle="dropdown">
-                                <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#"><i class="icon-plus-sign"></i> Do something</a></li>
-                                <li><a href="#"><i class="icon-cog"></i> Something else</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="span2 sp2 all-left">
-                    <div class="sidebar-nav">
-                        <ul class="nav nav-list">
-                            <li><a href="./"><i class="icon-home icon-aqua"></i> Home</a></li>
-							<li class="active"><a href="admin.php"><i class="icon-user icon-aqua"></i> Admin</a></li>
-                            <li><a href="about.html"><i class="icon-user icon-aqua"></i> About</a></li>
-                            <li><a href="contact.php"><i class="icon-envelope icon-aqua"></i> Contact</a></li>
-							<li><a href="tos.html"><i class="icon-exclamation-sign icon-aqua"></i> TOS</a></li>
-							<li><a href="privacy.html"><i class="icon-info-sign icon-aqua"></i> Privacy policy</a></li>
-                        </ul>
-                    </div>
-                </div><!--/span-->
-                <div class="span10 content">
-                    <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="/netbeans/bootstrap/js/excanvas.min.js"></script><![endif]-->
-                    <div id="content">
-                        <div class="span11 panel">
-                            <div class="panel-header">
-                                <i class="icon-list-alt icon-blue"></i>
-                                <h2>A total of <b><em><?php
-        print mysql_num_rows($rs);
-?></em></b> links shortened</h2>
-                            </div>
-			<table width="640">
-				<tr style="font-weight: bold">
-					<td width="250">Destination URL</td>
-					<td width="200">Shortened</td>
-					<td width="100">Added</td>
-					<td width="70">IP</td>
-					<td width="20">Remove</td>
-				</tr>
-			<?php
-        $i = 0;
-        while ($row = mysql_fetch_object($rs)) {
-            $i++;
-            if ($i % 2 != 0) {
-                $rowColor = "#e9e9e9";
-            } else {
-                $rowColor = "#cccccc";
-            }
-            print '<tr bgcolor="' . $rowColor . '">
-					  <td><a href="http://' . $row->destination . '" target="_blank">
-					  	' . wordwrap($row->destination, 40, '<br/>', true) . '
-					  	 </a>
-					  <td>http://' . $_SERVER['SERVER_NAME'] . '/' . $row->string . '</td>
-					  <td>' . date('d/m/Y H:iA', $row->added) . '</td>
-					  <td>' . long2ip($row->ip) . '</td>
-					  <td><a href="?delID=' . $row->linkID . '"><a class="btn btn-small btn-danger"><i class="icon-remove"></i></a></a></td>
-					  </tr>';
-        }
-?>
-			</table>
-			<?php
-    }
-}
-?>
-                            
-                    </div><!-- content -->
-                </div>
-            </div><!--/row-->
-			
-			
+    </div>		
+  </div>
+        
 	
 </div>
 	
--- headerc.php
+++ headerc.php
@@ -4,9 +4,10 @@
     <meta name="keywords" content="Your_Keywords" />
 	<meta name="author" content="IDVSE Developer, iDeveloper" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css" />
-    <link rel="stylesheet" type="text/css" href="./css/aquaadmin.css" />
+    <link rel="stylesheet" type="text/css" href="./css/materialize.min.css" />
+    <link rel="stylesheet" type="text/css" href="./css/materialize.css" />
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
     
     <!-- Change reCaptcha theme -->
@@ -16,3 +17,17 @@
 	 custom_theme_widget: 'recaptcha_widget'
       };
       </script>
+	  
+	<div class="navbar-fixed">
+		<nav>
+		  <div class="nav-wrapper">
+			<a href="./" class="brand-logo">iShortener</a>
+			<ul class="right hide-on-med-and-down">
+				<li><a href="about.php">About</a></li>
+				<li><a href="contact.php">Contact</a></li>
+				<li><a href="tos.php">TOS</a></li>
+				<li><a href="privacy.php">Privacy policy</a></li>
+			</ul>
+		  </div>
+		</nav>
+	</div>
